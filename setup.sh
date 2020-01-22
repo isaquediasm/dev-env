34a279777bd8cf85ad30aafe8dee00e75e1b8747
Isaque Dias <isaquediasm@gmail.com>
2020-01-22 15:54:56 +0100

    change file naming

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 echo "---------------------------------------------------------"
 echo "$(tput setaf 2) 1) Installing and setting up basics...$(tput sgr 0)"
 echo "---------------------------------------------------------"
-source scripts/setup-node.sh
+source scripts/setup-basics.sh
 
 echo "---------------------------------------------------------"
 echo "$(tput setaf 2) 2) Installing oh-my-zsh... $(tput sgr 0)"
